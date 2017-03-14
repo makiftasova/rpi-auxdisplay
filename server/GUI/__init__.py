da1dcc44162cf3b94ad9ac1b9ca4a5aadf1ca98c
Mehmet Akif TAŞOVA <makiftasova@gmail.com>
2017-03-14 19:42:21 +0300

    add different events for display GUI

--- server/GUI/__init__.py
+++ server/GUI/__init__.py
@@ -3,14 +3,21 @@ import logging
 import socketserver
 import threading
 
+from enum import Enum
+
 import tkinter as tk
 from tkinter import ttk
 
 from zeroconf import Zeroconf, ServiceInfo
 
+from .widgets import *
+
+__all__ = ["widgets"]
+
 
 class CustomTCPServer(socketserver.TCPServer):
-    def __init__(self, server_address, request_handler_class, bind_and_activate=True, gui=None, logger=None):
+    def __init__(self, server_address, request_handler_class, bind_and_activate=True, gui=None,
+                 logger=None):
         super().__init__(server_address, request_handler_class, bind_and_activate)
         self.gui = gui
         self.logger = logger
@@ -23,10 +30,18 @@ class ClientRequestHandler(socketserver.BaseRequestHandler):
 
         if self.server.gui is not None:
             self.server.gui.sock_data = data
-            self.server.gui.trigger_gui_update()
+            self.server.gui.trigger_gui_update(MainWindow.DataType.news)
 
 
 class MainWindow(tk.Tk):
+    DataType = Enum("DataType", "clock email news stock weather")
+
+    __EVENT_CLOCK_UPDATE = "<<UPDATE_CLOCK_EVENT>>"
+    __EVENT_EMAIL_UPDATE = "<<UPDATE_EMAIL_EVENT>>"
+    __EVENT_NEWS_UPDATE = "<<UPDATE_NEWS_EVENT>>"
+    __EVENT_STOCK_UPDATE = "<<UPDATE_STOCK_EVENT>>"
+    __EVENT_WEATHER_UPDATE = "<<UPDATE_WEATHER_EVENT>>"
+
     def __init__(self, ip=None):
         super().__init__()
         self.protocol('WM_DELETE_WINDOW', self.on_exit)
@@ -38,7 +53,8 @@ class MainWindow(tk.Tk):
         if ip is None:
             raise ValueError("Listen ip is None")
 
-        self.socket_server = CustomTCPServer((ip, 0), ClientRequestHandler, gui=self, logger=self.logger)
+        self.socket_server = CustomTCPServer((ip, 0), ClientRequestHandler, gui=self,
+                                             logger=self.logger)
         self.socket_thread = threading.Thread(target=self.socket_server.serve_forever)
         self.socket_thread.setDaemon(True)
         self.socket_thread.start()
@@ -50,20 +66,67 @@ class MainWindow(tk.Tk):
         self.__setup_zeroconf()
         self.logger.info("Zeroconf service registered.")
 
-        self.bind(sequence="<<SOCKET_EVENT>>", func=self.handle_gui_update)
+        self.bind(sequence=self.__EVENT_CLOCK_UPDATE, func=self.handle_clock_update)
+        self.bind(sequence=self.__EVENT_EMAIL_UPDATE, func=self.handle_email_update)
+        self.bind(sequence=self.__EVENT_NEWS_UPDATE, func=self.handle_news_update)
+        self.bind(sequence=self.__EVENT_STOCK_UPDATE, func=self.handle_stock_update)
+        self.bind(sequence=self.__EVENT_WEATHER_UPDATE, func=self.handle_weather_update)
+
         self.msg = tk.StringVar()
         self.message_box = ttk.Label(textvariable=self.msg)
         self.message_box.pack()
 
+        self.stock_label = SlidingLabel(master=self, separator="|")
+        self.stock_label.pack(fill='x', expand=True)
+
+        # TODO TEMP
+        self.stock_label.load_lines(["USD 3.65", "EUR 4.01"])
+
+        self.news_label = SlidingLabel(master=self)
+        self.news_label.pack(fill='x', expand=True)
+
+        # TODO temp line
+        self.news_label.load_lines(["very importtant news", "some other news", "just another one"])
+
         self.btn_quit = ttk.Button(text="Quit", command=self.on_exit)
         self.btn_quit.pack()
 
-    def trigger_gui_update(self):
-        self.event_generate("<<SOCKET_EVENT>>")
+        self.after(10000, self.__temp)
 
-    def handle_gui_update(self, event):
-        self.msg.set(self.sock_data)
-        self.logger.info("Data received: " + self.msg.get().strip())
+    # TODO remove after test
+    def __temp(self):
+        self.news_label.load_lines(["updated very important news",
+                                    "updated some other news",
+                                    "updated just another one"])
+
+    def trigger_gui_update(self, data_type):
+        if data_type == self.DataType.clock:
+            self.event_generate(self.__EVENT_CLOCK_UPDATE)
+        elif data_type == self.DataType.email:
+            self.event_generate(self.__EVENT_EMAIL_UPDATE)
+        elif data_type == self.DataType.news:
+            self.event_generate(self.__EVENT_NEWS_UPDATE)
+        elif data_type == self.DataType.stock:
+            self.event_generate(self.__EVENT_STOCK_UPDATE)
+        elif data_type == self.DataType.weather:
+            self.event_generate(self.__EVENT_WEATHER_UPDATE)
+        else:
+            self.logger.error("Unknown data type: " + data_type)
+
+    def handle_clock_update(self, event):
+        pass
+
+    def handle_email_update(self, event):
+        pass
+
+    def handle_news_update(self, event):
+        self.news_label.load_lines([self.sock_data])
+
+    def handle_stock_update(self, event):
+        pass
+
+    def handle_weather_update(self, event):
+        pass
 
     def on_exit(self):
         self.logger.info("Unregistering Zeroconf services...")
@@ -83,6 +146,7 @@ class MainWindow(tk.Tk):
         self.zeroconf_info = ServiceInfo("_scpi-raw._tcp.local.",
                                          "Auxiliary Display on {host}._scpi-raw._tcp.local.".format(
                                              host=socket.gethostname()),
-                                         address=socket.inet_aton(self.ip), port=self.port, properties=service_desc)
+                                         address=socket.inet_aton(self.ip), port=self.port,
+                                         properties=service_desc)
 
         self.zeroconf.register_service(self.zeroconf_info)
--- server/GUI/widgets.py
+++ server/GUI/widgets.py
@@ -3,7 +3,8 @@ from tkinter import ttk
 
 
 class SlidingLabel(ttk.Label):
-    def __init__(self, master=None, text=None, delay=100, **kw):
+    def __init__(self, separator=u"\u00B7", master=None, text=None, delay=200,
+                 font=("Helvetica", 50), **kw):
         """Construct a Ttk Label with parent master.
 
         STANDARD OPTIONS
@@ -15,13 +16,15 @@ class SlidingLabel(ttk.Label):
 
             anchor, background, font, foreground, justify, padding,
             relief, text, wraplength
-
-        :param master:
-        :param delay: sleep time before sliding to next position in ms
-        :param kw:
+        :param separator: separator character for list items
+        :param text: initial text (optional)
+        :param font: text font. default is ("Helvetica", 50)
+        :param delay: sleep time before sliding to next position in ms, default is 200ms
         """
         self.__str_var = tk.StringVar()
-        super(SlidingLabel, self).__init__(master, textvariable=self.__str_var, **kw)
+        super(SlidingLabel, self).__init__(master, font=font,
+                                           textvariable=self.__str_var, **kw)
+        self.separator = separator
         self.delay = delay  # wait time before sliding to next position, in ms
         self.text = text if text else ""
         self.__str_var.set(self.text)
@@ -34,7 +37,7 @@ class SlidingLabel(ttk.Label):
             self.master.after(self.delay, self.__update_text_position)
 
     def load_lines(self, line_list):
-        sep = " " + u"\u00B7" + " "
+        sep = " " + self.separator + " "
         txt = sep.join(line_list)
         txt += sep
         self.text = txt
